b7eb402573fc819cf67c6f9c58aee400e40a3eae
Bak-JH <bakjh.6280@gmail.com>
2023-02-13 11:48:04 +0900

    add linear accel

--- electron/actions.ts
+++ electron/actions.ts
@@ -29,21 +29,21 @@ abstract class Action {
     //method
     public abstract run():void;
 }
- 
+
 abstract class GPIOAction extends Action {
     //variable
     private readonly _pin!:GPIOPin 
-    protected _pinObj!:Gpio;
+    // protected _pinObj!:Gpio;
 
     //getter
     get pin() : GPIOPin { return this._pin; }
-    get pinObj() : Gpio { return this._pinObj; }
+    // get pinObj() : Gpio { return this._pinObj; }
 
     //method
     constructor(pin:GPIOPin) { 
         super(); 
         this._pin = pin; 
-        this._pinObj = new Gpio(pin, "out");
+        // this._pinObj = new Gpio(pin, "out");
     }
 }
 abstract class PWMAction extends Action {
@@ -73,7 +73,7 @@ class GPIOEnable extends GPIOAction {
         this._enable = enable; 
     }
     public run() {
-        this.pinObj.writeSync(toBinaryValue(this._enable));
+        // this.pinObj.writeSync(toBinaryValue(this._enable));
         console.log("GPIOAction: GPIOEnable(" + this.pin + "," + toBinaryValue(this._enable) + ")");
     }
 }
@@ -92,7 +92,7 @@ class PWMEnable extends PWMAction {
     }
 
     public async run() {
-        console.log("GPIOAction: PWMEnable " + this.enable);
+        console.log("PWMAction: PWMEnable " + this.enable);
 
         if (PWMWorkers[this.pin] == null) {
             PWMWorkers[this.pin] = await new Worker(__dirname + '/worker/pwmWorker.js');
@@ -147,46 +147,29 @@ class PWMSetDuty extends PWMAction {
 
 class PWMLinearAccel extends PWMAction {
     //variable
-    private readonly _minSpeed!:number
-    private readonly _maxSpeed!:number
+    private readonly _startSpeed!:number
+    private readonly _targetSpeed!:number
     private readonly _duration!:number;
-    private readonly _timeStep = 1000;
+
+    get startSpeed() : number { return this._startSpeed; }
+    get targetSpeed() : number { return this._targetSpeed; }
+    get duration() : number { return this._duration; }
 
     //method
-    constructor(pin:PWMPin, minSpeed:number, maxSpeed:number, duration:number) {
+    constructor(pin:PWMPin, startSpeed:number, targetSpeed:number, duration:number) {
         super(pin);
 
-        this._minSpeed = minSpeed;
-        this._maxSpeed = maxSpeed;
+        this._startSpeed = startSpeed;
+        this._targetSpeed = targetSpeed;
         this._duration = duration;
-    }
-
-    stepTransform():Array<Action>
-    {
-        const stepCnt = Math.ceil(this._duration / this._timeStep);
-        const spdInc = (this._maxSpeed - this._minSpeed)/ stepCnt;
-        const actions:Array<Action> = [];
-
-        for(var i = 0; i < stepCnt - 1; ++i)
-        {
-            actions.push(new PWMSetDuty(this.pin, this._minSpeed + spdInc*i));
-            actions.push(new Wait(this._timeStep));
-        }
-        //we add last step by hand to ensure float error as following:
-        //1. total duration must equal to actual duration
-        //2. final speed must be exact equal to given end speed
-        actions.push(new PWMSetDuty(this.pin, this._maxSpeed));
-        if(this._duration < this._timeStep)
-        actions.push(new Wait(this._duration));
-        else
-        actions.push(new Wait(this._duration - this._timeStep * (stepCnt - 1)));
-        return actions;
     }
 
     public async run() {
         console.log("PWMAction: PWMLinearAccel");
-        await PWMWorkers[this.pin].postMessage("linearAccel");
+
+        await PWMWorkers[this.pin].postMessage(["linearAccel", this.startSpeed, this.targetSpeed, this.duration]);
     }
+    
 }
 
 const wait = (timeToDelay:number) => new Promise((resolve) => setTimeout(resolve, timeToDelay));
--- electron/worker/pwmWorker.ts
+++ electron/worker/pwmWorker.ts
@@ -1,18 +1,22 @@
-import { Gpio } from 'onoff';
 import {parentPort} from 'worker_threads';
 import { wait } from '../actions';
+import { Gpio } from 'onoff';
+
+enum WorkerMethod{
+    SetPin = "setPin",
+    SetPeriod = "setPeriod",
+    SetDuty = "setDuty",
+    LinearAccel = "linearAccel"
+}
+
+const ON = 1;
+const OFF = 0;
 
 let period:number = 0;
 let duty:number = 0;
 
 let pin:number;
 let gpioObj:Gpio;
-
-enum WorkerMethod{
-    SetPin = "setPin",
-    SetPeriod = "setPeriod",
-    SetDuty = "setDuty"
-}
 
 if(parentPort){
     parentPort.on("message",(value)=>{
@@ -22,7 +26,7 @@ if(parentPort){
         {
             case WorkerMethod.SetPin:
                 pin = value[1];
-                gpioObj = new Gpio(pin, 'out');
+                // gpioObj = new Gpio(pin, 'out');
                 break;
             case WorkerMethod.SetPeriod:
                 period = value[1];
@@ -30,25 +34,60 @@ if(parentPort){
             case WorkerMethod.SetDuty:
                 duty = value[1];
                 break;
+            case WorkerMethod.LinearAccel:
+                accelLoop(value[1], value[2], value[3]);
+                break;
         }
 
         console.log("pin: " + pin + " period: " + period + " duty: " + duty);
 
-        if(period > 0 && duty > 0 && duty < 1 && gpioObj)
+        if(value[0] != WorkerMethod.LinearAccel &&
+            period > 0 && duty > 0 && duty < 1 && gpioObj)
             loop();
     })
 }
 
-
 async function loop() {
-    console.log("pin " + pin + " start run");
+    console.log("pin " + pin + " start loop");
+
     while(true)
     {
         await wait(period * Math.abs(1 - duty));
-        gpioObj.writeSync(Gpio.HIGH);
+        // console.log("1")
+        gpioObj.writeSync(ON);
         await wait(period * duty);
-        gpioObj.writeSync(Gpio.LOW);
+        // console.log("0")
+        gpioObj.writeSync(OFF);
     }
+}
+
+async function accelLoop(startDuty:number, targetDuty:number, totalTime:number) {
+    console.log("pin " + pin + " start accel loop");
+ 
+    const timeStep = 1000;
+    const stepCnt = Math.ceil(totalTime / timeStep);
+    const spdInc = (targetDuty - startDuty)/ stepCnt;
+    duty = startDuty;
+    
+
+    for(let step = 0; step < stepCnt; ++step)
+    {
+        for (let loopCount = 0; loopCount < timeStep / period; ++loopCount) {
+            await wait(period * Math.abs(1 - duty));
+            // console.log("1")
+            gpioObj.writeSync(ON);
+            await wait(period * duty);
+            // console.log("0")
+            gpioObj.writeSync(OFF);
+        }
+
+        if (duty < targetDuty){
+            duty += spdInc;
+            // console.log("accel: " + duty);
+        }
+    }
+
+    await loop();
 }
 
 export {WorkerMethod}
